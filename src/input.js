632a064cf9244fe4656dff28d7d3831fea6063bf
danny-laa <46824499+danny-laa@users.noreply.github.com>
2021-03-05 23:23:51 +0800

    Add files via upload

--- src/input.js
+++ src/input.js
@@ -1,3 +1,4 @@
-const randomString = [0, 1, 2].includes(Math.random()) > 0.5 ? 'abc' : 'def';
-const fn = () => console.log(`foo ${randomString}`);
+const randomString = () =>
+  [1, 2].includes(Math.ceil(Math.random() * 4)) ? 'abc' : 'def';
+const fn = () => console.log(`foo ${randomString()}`);
 fn();
